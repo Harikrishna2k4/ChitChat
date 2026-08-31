5db5ff0faebd683b772fbf33da72bb2351adc90d
Hari Krishna R <192508744+Harikrishna2k4@users.noreply.github.com>
2026-08-31 06:56:55 +0530

    initial setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
-import messageRoute from "./routes/message.route.js"
+import messageRoute from "./routes/message.route.js";
+import path from "path";
+
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (_,res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
