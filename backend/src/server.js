d60953f611deee6f50c0c6c9b6d392a9ff05c9df
Hari Krishna R <192508744+Harikrishna2k4@users.noreply.github.com>
2026-08-31 07:41:58 +0530

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use("/api/message", messageRoute);
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "frontend", "dist")));
     app.get("*", (_,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     })
 }
 
