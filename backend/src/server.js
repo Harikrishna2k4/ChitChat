3857ff2f3ad9d02111a9f31fbfbfefde2a042f18
Hari Krishna R <192508744+Harikrishna2k4@users.noreply.github.com>
2026-08-31 07:45:14 +0530

    commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,9 +13,9 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.use(express.static(path.join(__dirname,"..", "frontend", "dist")));
     app.get("*", (_,res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname,"..", "frontend", "dist", "index.html"));
     })
 }
 
